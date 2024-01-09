e668c8ebd5e80956453f1f71af962e05a880863c
Flynn <156088772+flyxst@users.noreply.github.com>
2024-01-09 22:59:26 +0300

    Create stringconcatenation.cpp

---
+++ stringconcatenation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main () {
+  string firstName = "Flynn ";
+  string lastName = "Arikan";
+  string fullName = firstName + lastName;
+  cout << fullName;
+  return 0;
+}
